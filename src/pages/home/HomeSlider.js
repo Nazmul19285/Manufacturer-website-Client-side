8ad34577988e034e8e93cc91550a622ca5b43df1
Md. Nazmul Hossain <60980415+Nazmul19285@users.noreply.github.com>
2022-10-26 16:32:48 +0600

    Update HomeSlider.js

--- src/pages/home/HomeSlider.js
+++ src/pages/home/HomeSlider.js
@@ -11,14 +11,14 @@ const HomeSlider = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/PtdB42L/New-Project-1.jpg" alt='slide1' className="w-full"/>
+                <img src="https://i.postimg.cc/g0MzBRbZ/red-sports-bike.jpg" alt='slide1' className="w-full"/>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle"> ❮ </a>
                     <a href="#slide3" className="btn btn-circle"> ❯ </a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/rpcN9ms/New-Project.jpg" alt='slide1' className="w-full"/>
+                <img src="https://i.postimg.cc/SxdYknpy/white-bicycle-standing-park-morning-fitness-loneliness.jpg" alt='slide1' className="w-full"/>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle"> ❮ </a>
                     <a href="#slide1" className="btn btn-circle"> ❯ </a>
